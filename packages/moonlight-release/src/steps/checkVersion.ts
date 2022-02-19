3df8638ebf78b2acf89363f1750d7234822679f6
Clément BERARD <gitlab@berard.dev>
2022-02-20 00:54:57 +0100

    up

---
+++ packages/moonlight-release/src/steps/checkVersion.ts
@@ -0,0 +1,11 @@
+import semver from 'semver';
+import { inputsGhaParams } from '@moon-scripts/gha-helper';
+
+(async () => {
+    console.log('CHECCKK VERDDUDGHUIJDHUDHDIUO');
+    console.log('inputs', inputsGhaParams());
+    console.log('inputsVER', inputsGhaParams('versionToRelease'));
+
+
+
+})()
