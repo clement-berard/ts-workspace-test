c459ea5158e26947bf7ecbcc6cd0cf907eb46709
Clément BERARD <gitlab@berard.dev>
2022-02-20 00:34:35 +0100

    up

--- packages/moonlight-release/src/index.ts
+++ packages/moonlight-release/src/index.ts
@@ -2,6 +2,11 @@ import * as core from '@actions/core'
 import { inputsGhaParams } from '@moon-scripts/gha-helper';
 import { renderPrettyJson } from '@moon-scripts/cli-printer';
 
+
+export const getVersionToRelease = () => {
+    inputsGhaParams('versionToRelease');
+}
+
 (() => {
 
     console.log('inputs', inputsGhaParams());
