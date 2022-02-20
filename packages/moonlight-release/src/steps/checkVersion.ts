ef2708bae183516a8f286c5bd7ea3c4c17f7c317
Clément BERARD <gitlab@berard.dev>
2022-02-20 01:05:55 +0100

    up

--- packages/moonlight-release/src/steps/checkVersion.ts
+++ packages/moonlight-release/src/steps/checkVersion.ts
@@ -2,10 +2,15 @@ import semver from 'semver';
 import { inputsGhaParams } from '@moon-scripts/gha-helper';
 
 (async () => {
-    console.log('CHECCKK VERDDUDGHUIJDHUDHDIUO');
-    console.log('inputs', inputsGhaParams());
     console.log('inputsVER', inputsGhaParams('versionToRelease'));
+    const inputVersion = inputsGhaParams('versionToRelease');
 
+    const isValid =  semver.valid(inputVersion);
 
+    if(isValid){
+        process.exit(0);
+    } else {
+        process.exit(1);
+    }
 
 })()
